f482dd4535d24c0d1f0fa7c670f697d9de192eac
YohiSom <96017083+YohiSom@users.noreply.github.com>
2022-09-11 15:38:24 +0300

    small change debugged

--- js/calender.js
+++ js/calender.js
@@ -1,6 +1,11 @@
 function calender(arr) {
   let newObj = {};
   let newArr = [];
+
+  arr.sort((a, b) => {
+    return a.start - b.start;
+  });
+
   for (let i = 0; i < arr.length; i++) {
     let count = 1;
     let width = 600;
@@ -84,11 +89,8 @@ function calender(arr) {
 
 function margin() {
   let arr = calender([
-    { id: 1, start: 90, end: 130 }, // an event from 10:30am to 11.10am
-    { id: 2, start: 105, end: 135 }, // an event from 10:45am to 11:15am
-    { id: 3, start: 120, end: 240 }, // an event from 11:00am to 1:00pm
-    { id: 4, start: 180, end: 260 }, // an event from 12:00pm to 1:20pm
-    { id: 5, start: 500, end: 560 }, // an event from 5:20pm to 6:20pm
+    { id: 0, start: 220, end: 460 },
+    { id: 1, start: 120, end: 180 },
   ]);
 
   let newArr = [];
